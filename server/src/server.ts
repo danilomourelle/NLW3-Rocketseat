9f69ee4dff49bced91dd8a4e965dd42e070fe944
Danilo Mourelle <danilomourelle@outlook.com>
2020-10-14 10:43:52 -0300

    database creation

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,12 +1,25 @@
 import express from 'express';
+import path from 'path';
+import chalk from 'chalk';
+import cors from 'cors';
+import { AddressInfo } from 'net';
+import 'express-async-errors'
+
+import './database/connection';
+import errorHandler from './errors/handler'
+import routes from './routes';
 
 const app = express();
+app.use(cors());
 
-app.get('/users', () => {
+app.use(express.json());
+app.use(routes);
+app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
+app.use(errorHandler);
 
+const server = app.listen(process.env.PORT || 5000, () => {
+  const address = server.address() as AddressInfo;
+  console.log(chalk.yellow(`\nServidor on-line em http://localhost/${address.port}`));
 })
 
-const port = process.env.PORT || 5000
-app.listen(port, () => {
-  console.log(`Servidor on-line em http://localhost/${port}`)
-})
+export default app
